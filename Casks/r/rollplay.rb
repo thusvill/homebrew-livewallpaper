a6265da6772335d5a6861bd4ac53e2308fa97017
thusvill <thusvill@gmail.com>
2025-05-27 13:00:10 +0530

    Add RollPlay cask

---
+++ Casks/r/rollplay.rb
@@ -0,0 +1,23 @@
+cask "rollplay" do
+  version "1.2.4"
+  sha256 :no_check
+
+  url "https://github.com/thusvill/LiveWallpaperMacOS/releases/download/V#{version}/RollPlay-Silicon.dmg"
+  name "RollPlay"
+  desc "Open-source live wallpaper application"
+  homepage "https://github.com/thusvill/LiveWallpaperMacOS"
+
+  depends_on macos: ">= :sequoia"
+
+  app "RollPlay.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args:          ["-d", "com.apple.quarantine", "/Applications/RollPlay.app"],
+                   sudo:          false,
+                   print_stderr:  false,
+                   fail_if_error: false
+  end
+
+  zap trash: "/Applications/RollPlay.app"
+end
