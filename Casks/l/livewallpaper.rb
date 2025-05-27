f98be816d8cd613eef91b265a71f59416f1c556e
thusvill <thusvill@gmail.com>
2025-05-27 19:32:38 +0530

    rebranded

--- Casks/l/livewallpaper.rb
+++ Casks/l/livewallpaper.rb
@@ -1,26 +1,26 @@
-cask "rollplay" do
+cask "livewallpaper" do
   version "1.2.4"
   sha256 :no_check
 
-  url "https://github.com/thusvill/LiveWallpaperMacOS/releases/download/V#{version}/RollPlay-Silicon.dmg"
-  name "RollPlay"
+  url "https://github.com/thusvill/LiveWallpaperMacOS/releases/download/V#{version}/LiveWallpaper-Silicon.dmg"
+  name "LiveWallpaper"
   desc "Open-source live wallpaper application"
   homepage "https://github.com/thusvill/LiveWallpaperMacOS"
 
   depends_on macos: ">= :sequoia"
 
-  app "RollPlay.app"
+  app "LiveWallpaper.app"
 
   postflight do
     # macOS may not always apply quarantine attribute; ignore error if missing
 
     system_command "/usr/bin/xattr",
-                   args:         ["-d", "com.apple.quarantine", "/Applications/RollPlay.app"],
+                   args:         ["-d", "com.apple.quarantine", "/Applications/LiveWallpaper.app"],
                    sudo:         false,
                    print_stderr: false
   rescue
     opoo "com.apple.quarantine attribute not found or could not be removed"
   end
 
-  zap trash: "/Applications/RollPlay.app"
+  zap trash: "/Applications/LiveWallpaper.app"
 end
